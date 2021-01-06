03e56dbe1dad7318d78a3937de4c37d96b77a8d6
Emerson Silva <pereiradasilva.emerson@gmail.com>
2021-01-05 23:48:34 -0300

    Project created

---
+++ backend/src/test/java/br/dev/mission/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mission.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
