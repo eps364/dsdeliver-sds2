6660deeeb730d1f423b131fcfd4b7d09f87727c6
Emerson Silva <pereiradasilva.emerson@gmail.com>
2021-01-06 01:56:07 -0300

    GET /orders

---
+++ backend/src/main/java/br/dev/mission/dsdeliver/services/OrderService.java
@@ -0,0 +1,27 @@
+package br.dev.mission.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.dev.mission.dsdeliver.dto.OrderDTO;
+import br.dev.mission.dsdeliver.entities.Order;
+import br.dev.mission.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+}
