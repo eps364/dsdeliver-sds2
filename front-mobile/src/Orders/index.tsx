2d8e4807ee0f430487b221f5a5bab5d2312e04b5
Emerson Silva <pereiradasilva.emerson@gmail.com>
2021-01-13 01:33:06 -0300

    Add axios

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Alert } from 'react-native';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import {fetchOrders} from '../api'
+import {Order} from '../type'
 
 function Orders() {
+    const [orders,setOrders] = useState<Order[]>([])
+    useEffect(()=> {
+        fetchOrders()
+        .then(response => setOrders(response.data))
+        .catch(() => Alert.alert('Erro ao buscar os pedidos!'))
+    },[])
     return (
         <>
             <Header />
             <ScrollView style={styles.container}>
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
                 <OrderCard />
             </ScrollView>
         </>
