d66d77fcf0e7b4f262a781829f850c3b6e7fecbe
Emerson Silva <pereiradasilva.emerson@gmail.com>
2021-01-13 01:04:38 -0300

    Add OrderCard

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import { StyleSheet,Text,View } from 'react-native'
-import Header from '../Header'
+import React from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
 
 function Orders() {
     return (
         <>
-          <Header />
-          <View>
-            <Text>Listagem de Pedidos</Text>
-            
-          </View>
+            <Header />
+            <ScrollView style={styles.container}>
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+                <OrderCard />
+            </ScrollView>
         </>
-  )
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    }
+});
 
 export default Orders
